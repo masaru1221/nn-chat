1e8e0302ea76cef2789de0afab4aab776c67c721
masaru1221 <masarudd2006@gmail.com>
2026-06-04 11:03:45 +0900

    Add files via upload

---
+++ index.js
@@ -0,0 +1,32 @@
+'use strict';
+const http = require('node:http');
+const auth = require('http-auth');
+const router = require('./lib/router');
+
+const basic = auth.basic({
+  realm: 'Enter username and password.',
+  file: './users.htpasswd'
+});
+
+const server = http.createServer(basic.check((req, res) => {
+  Promise.resolve(router.route(req, res)).catch((e) => {
+    console.error('Request Error', e);
+    if (!res.headersSent) {
+      res.writeHead(500, {
+        'Content-Type': 'text/plain; charset=utf-8'
+      });
+    }
+    res.end('サーバーエラーが発生しました');
+  });
+}))
+  .on('error', e => {
+    console.error('Server Error', e);
+  })
+  .on('clientError', e => {
+    console.error('Client Error', e);
+  });
+
+const port = process.env.PORT || 8000;
+server.listen(port, '0.0.0.0', () => {
+  console.info(`Listening on ${port}`);
+});
